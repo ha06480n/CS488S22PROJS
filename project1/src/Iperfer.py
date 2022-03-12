992e7b6f2ae7f7e289fa040d51e5bf5a14ad1b96
ha06480n <77877650+ha06480n@users.noreply.github.com>
2022-03-11 20:29:14 -0800

    Add files via upload

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -10,13 +10,14 @@ host = str(sys.argv[1])  # The server's hostname or IP address
 port = int(sys.argv[2])  # The port used by the server
 myTime = int(sys.argv[3]) # The duration in seconds for which data is generated
 
+host = socket.gethostbyname(host)
 if((port < 1024) or (port > 65535)): # Port out of range
     print("Error: port number must be in the range 1024 to 65535")
     quit()
 
 address = (host, port)
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4, TCP socket
-clientSocket.connect(('192.168.198.1',1200))
+clientSocket.connect(address)
 message = bytearray(1000) #Message 1000 bytes long of 0
 counter = 0 #Counter for number of messages sent
 
