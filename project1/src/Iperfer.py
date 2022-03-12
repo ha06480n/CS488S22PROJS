ef7389df0f20f4efcffae28ff15c76ec515ff59e
ha06480n <77877650+ha06480n@users.noreply.github.com>
2022-03-11 20:19:24 -0800

    Add files via upload

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -16,7 +16,7 @@ if((port < 1024) or (port > 65535)): # Port out of range
 
 address = (host, port)
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4, TCP socket
-clientSocket.connect(address)
+clientSocket.connect(('192.168.198.1',1200))
 message = bytearray(1000) #Message 1000 bytes long of 0
 counter = 0 #Counter for number of messages sent
 
